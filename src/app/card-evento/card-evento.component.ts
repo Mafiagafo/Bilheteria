1e0911646148e61c60a537568ed7c3aa3e8fda23
mafiagafo <mafiagafo@hotmail.com>
2022-03-15 22:53:39 -0300

    Ajustado Tamanho dos Cards e Botões

--- src/app/card-evento/card-evento.component.ts
+++ src/app/card-evento/card-evento.component.ts
@@ -3,22 +3,21 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Evento } from '../models/evento';
 
 
+
 @Component({
   selector: 'card-evento',
   templateUrl: './card-evento.component.html',
   styleUrls: ['./card-evento.component.css']
 })
 export class CardEventoComponent {
-  @Input('evento')
-  evento!: Evento;
+  @Input('evento')  evento: Evento;
 
   constructor(private carrinhoService: CarrinhoService) { }
 
   addCarrinho(evento: Evento){
-    let carrinhoId = localStorage.getItem('carrinhoId');
-    if (!carrinhoId){
-      this.carrinhoService.criar().then(result => 
-        localStorage.setItem('carrinhoId', ''));
-      };
-    }
+    
+    return this.carrinhoService.Carrinho(evento);
+
+    
   }
+}
--- src/app/carrinho.service.ts
+++ src/app/carrinho.service.ts
@@ -1,3 +1,4 @@
+import { Evento } from './models/evento';
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 
@@ -6,12 +7,25 @@ import { AngularFireDatabase } from '@angular/fire/compat/database';
 })
 export class CarrinhoService {
 
-  constructor(private db: AngularFireDatabase) { 
-    
-  }
-  criar(){
+  constructor(private db: AngularFireDatabase) {  }
+  private criar(){
     return this.db.list('/carrinho').push({
       dataCarrinho: new Date().getTime()
     })
+  }
+
+  private selecionaCarrinho (carrinhoId: string){
+    return this.db.object('/carrinho/'+ carrinhoId)
+  }
+
+Carrinho(evento: Evento){
+  let carrinhoId = localStorage.getItem('carrinhoId');
+    if (!carrinhoId){
+      this.criar().then(result => {
+        localStorage.setItem('carrinhoId', String(result.key));
+
+        return this.selecionaCarrinho(String(result.key));
+      });
+    }
   }
 }
